42415c52c4d83dce2a75f0615a683b8b3dc278ee
JPrakash <JPrakash@localhost>
2012-06-13 19:36:14 +0530

    TempCommit

--- WebContent/WEB-INF/Source/bsuite/weber/security/Profile.java
+++ WebContent/WEB-INF/Source/bsuite/weber/security/Profile.java
@@ -1,20 +1,31 @@
 package bsuite.weber.security;
 
+import java.io.IOException;
 import java.util.ArrayList;
+
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import lotus.domino.Document;
+import lotus.domino.NotesException;
+
 
 import bsuite.weber.jsonparsing.*;
 
 import bsuite.weber.model.BsuiteWorkFlow;
+
+import bsuite.weber.relationship.Association;
 
 public class Profile extends BsuiteWorkFlow {
 	private ProfileJson profileJson;
 
 	public Profile() {
 		super();
-		this.profileJson = null;
+		this.profileJson = getProfileJsonObject();
 	}
 
-	public ArrayList getVisibleModules(){
+	public ArrayList<Module> getVisibleModules(){
 		ArrayList<Module> vModules = new ArrayList<Module>();
 		for(Module module:profileJson.getModules()){
 			if(module.getTabvis().equals("1")){
@@ -24,7 +35,7 @@ public class Profile extends BsuiteWorkFlow {
 		return vModules;
 	}
 
-	public ArrayList getCreatableEntities(String moduleName){
+	public ArrayList<Entity> getCreatableEntities(String moduleName){
 		ArrayList<Entity> cEntities = new ArrayList<Entity>();
 		Module mod = getModule(moduleName);
 		
@@ -37,8 +48,8 @@ public class Profile extends BsuiteWorkFlow {
 	}
 
 
-	public ArrayList getReadonlyFields(String moduleName, String entityName){
-		ArrayList rFields = new ArrayList();
+	public ArrayList<Field> getReadonlyFields(String moduleName, String entityName){
+		ArrayList<Field> rFields = new ArrayList<Field>();
 		Entity entity = getEntity(moduleName,entityName);
 		for(Field f:entity.getFields()){
 			if(f.getReadonly().equals("1")){
@@ -50,8 +61,8 @@ public class Profile extends BsuiteWorkFlow {
 	}
 	
 	
-	public ArrayList getEditableFields(String moduleName, String entityName){
-		ArrayList eFields = new ArrayList();
+	public ArrayList<Field> getEditableFields(String moduleName, String entityName){
+		ArrayList<Field> eFields = new ArrayList<Field>();
 		Entity entity = getEntity(moduleName,entityName);
 		for(Field f:entity.getFields()){
 			if(f.getReadonly().equals("0")){
@@ -62,9 +73,9 @@ public class Profile extends BsuiteWorkFlow {
 		
 	}
 
-	public ArrayList getVisibleFeatures(String moduleName, String entityName){
+	public ArrayList<Feature> getVisibleFeatures(String moduleName, String entityName){
 		Entity entity = getEntity(moduleName,entityName);
-		ArrayList vFeatures = new ArrayList();
+		ArrayList<Feature> vFeatures = new ArrayList<Feature>();
 		for(Feature f:entity.getFeatures()){
 			if(f.getVisible().equals("1")){
 				vFeatures.add(f);
@@ -109,18 +120,18 @@ public class Profile extends BsuiteWorkFlow {
 		}
 		return null;
 	}
-	public ArrayList getVisibleModulesNames(){
-		ArrayList<Module> vModules = new ArrayList<Module>();
+	public ArrayList<String> getVisibleModulesNames(){
+		ArrayList<String> vModules = new ArrayList<String>();
 		for(Module module:profileJson.getModules()){
 			if(module.getTabvis().equals("1")){
-				vModules.add(module);
+				vModules.add(module.getModuleName());
 			}
 		}
 		return vModules;
 	}
 
-	public ArrayList getCreatableEntitiesNames(String moduleName){
-		ArrayList cEntities = new ArrayList();
+	public ArrayList<String> getCreatableEntitiesNames(String moduleName){
+		ArrayList<String> cEntities = new ArrayList<String>();
 		Module mod = getModule(moduleName);
 		
 		for(Entity entity:mod.getEntities()){
@@ -132,8 +143,8 @@ public class Profile extends BsuiteWorkFlow {
 	}
 
 
-	public ArrayList getReadonlyFieldsNames(String moduleName, String entityName){
-		ArrayList rFields = new ArrayList();
+	public ArrayList<String> getReadonlyFieldsNames(String moduleName, String entityName){
+		ArrayList<String> rFields = new ArrayList<String>();
 		Entity entity = getEntity(moduleName,entityName);
 		for(Field f:entity.getFields()){
 			if(f.getReadonly().equals("1")){
@@ -145,8 +156,8 @@ public class Profile extends BsuiteWorkFlow {
 	}
 	
 	
-	public ArrayList getEditableFieldsNames(String moduleName, String entityName){
-		ArrayList eFields = new ArrayList();
+	public ArrayList<String> getEditableFieldsNames(String moduleName, String entityName){
+		ArrayList<String> eFields = new ArrayList<String>();
 		Entity entity = getEntity(moduleName,entityName);
 		for(Field f:entity.getFields()){
 			if(f.getReadonly().equals("0")){
@@ -157,9 +168,9 @@ public class Profile extends BsuiteWorkFlow {
 		
 	}
 
-	public ArrayList getVisibleFeaturesNames(String moduleName, String entityName){
+	public ArrayList<String> getVisibleFeaturesNames(String moduleName, String entityName){
 		Entity entity = getEntity(moduleName,entityName);
-		ArrayList vFeatures = new ArrayList();
+		ArrayList<String> vFeatures = new ArrayList<String>();
 		for(Feature f:entity.getFeatures()){
 			if(f.getVisible().equals("1")){
 				vFeatures.add(f.getFeatureName());
@@ -182,8 +193,8 @@ public class Profile extends BsuiteWorkFlow {
 	}
 	
 	
-	public ArrayList getVisibleFieldsNames(String moduleName, String entityName){
-		ArrayList vFields = new ArrayList();
+	public ArrayList<String> getVisibleFieldsNames(String moduleName, String entityName){
+		ArrayList<String> vFields = new ArrayList<String>();
 		Entity entity = getEntity(moduleName,entityName);
 		for(Field f:entity.getFields()){
 			if(f.getVisible().equals("1")){
@@ -193,8 +204,8 @@ public class Profile extends BsuiteWorkFlow {
 		
 		return vFields;
 	}
-	public ArrayList getVisibleFields(String moduleName, String entityName){
-		ArrayList vFields = new ArrayList();
+	public ArrayList<Field> getVisibleFields(String moduleName, String entityName){
+		ArrayList<Field> vFields = new ArrayList<Field>();
 		Entity entity = getEntity(moduleName,entityName);
 		for(Field f:entity.getFields()){
 			if(f.getVisible().equals("1")){
@@ -205,5 +216,29 @@ public class Profile extends BsuiteWorkFlow {
 		return vFields;
 	}
 	
+	private ProfileJson getProfileJsonObject(){
+		Association as = new Association();
+		Document profDoc = as.getAssociatedProfile(currentuser.getBsuiteuser());
+		ObjectMapper mapper = new ObjectMapper();
+		String jsonString="";
+		try {
+			jsonString = profDoc.getItemValueString("JsonString");
+		} catch (NotesException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			return mapper.readValue(jsonString, ProfileJson.class);
+		} catch (JsonParseException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+				
+	}
 	
 }
